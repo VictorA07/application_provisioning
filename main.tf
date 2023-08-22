dc7895efbb912cda5e824e6c41e71ce78fc81de1
VictorA07 <victor.adepoju@cloudhight.com>
2023-08-22 22:29:43 +0100

    modifying the ec2 userdata

--- main.tf
+++ main.tf
@@ -212,18 +212,24 @@ resource "aws_db_subnet_group" "cap-db-sn" {
 }
 
 #Creating Database
-resource "aws_db_instance" "cap-db" {
+resource "aws_db_instance" "capdb" {
+    identifier = "capdb"
     allocated_storage = 10
     db_name = "capdb"
     engine = "mysql"
     engine_version = "8.0.33"
     instance_class = "db.t3.micro"
+    publicly_accessible = false
     username = "Admin"
     password = "Admin123"
     parameter_group_name = "default.mysql8.0"
     db_subnet_group_name = "cap-db-sn"
     vpc_security_group_ids = [aws_security_group.capbackend-SG.id]
     skip_final_snapshot = true
+    port = 3306
+    max_allocated_storage = 1000
+    apply_immediately = true
+    multi_az = true
     
 }
 
@@ -240,7 +246,7 @@ resource "aws_s3_bucket" "capmedia" {
 resource "aws_s3_bucket_ownership_controls" "capmedia-ct" {
     bucket = aws_s3_bucket.capmedia.id
     rule {
-      object_ownership = "BucketOwnerPreferred"
+      object_ownership = "BucketOwnerEnforced"
     }
     depends_on = [ aws_s3_bucket_public_access_block.capmedia-pab ]
   
@@ -308,7 +314,7 @@ resource "aws_s3_bucket_acl" "cap-log-acl" {
 resource "aws_s3_bucket_ownership_controls" "cap-log-ct" {
     bucket = aws_s3_bucket.cap-log.id
     rule {
-      object_ownership = "BucketOwnerPreferred"
+      object_ownership = "BucketOwnerEnforced"
     }
     #depends_on = [ aws_s3_bucket_acl.cap-log-acl ]
   
@@ -385,3 +391,56 @@ resource "aws_iam_instance_profile" "cap-S3IAM" {
 }   
 
 #Wordpress configuration and spinning up instance
+## Creating keypair
+resource "aws_key_pair" "keypair" {
+    key_name = "capstone-keypair"
+    #public_key = file("~/devops/ssh_key_pair")
+    public_key = file("~/devops/set-16-keypair.pem")
+}
+
+#Creating webserver
+resource "aws_instance" "webserver" {
+
+  ami = "ami-0ca5ef73451e16dc1"
+  instance_type = "t2.micro"
+  key_name = "keypair"
+  subnet_id = aws_subnet.cappub1.id
+  vpc_security_group_ids = [aws_security_group.capfrontend-SG.id]
+  iam_instance_profile = "aws_iam_instance_profile.cap-S3IAM.id"
+  associate_public_ip_address = true
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum upgrade -y
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    sudo yum install httpd php php-mysqlnd -y
+    cd /var/www/html
+    echo "This is a test file" > indextest.html
+    sudo yum install wget -y
+    wget https://wordpress.org/wordpress-6.1.1.tar.gz
+    tar -xzf wordpress-6.1.1.tar.gz
+    cp -r wordpress/* /var/www/html/
+    rm -rf wordpress
+    rm -rf wordpress-6.1.1.tar.gz
+    chmod -R 755 wp-content
+    chown -R apache:apache wp-content
+    cd /var/www/html && mv wp-config-sample.php wp-config.php
+    sed -i "s@define( 'DB_NAME', 'database_name_here' )@define( 'DB_NAME', 'capdb' )@g" /var/www/html/wp-config.php
+    sed -i "s@define( 'DB_USER', 'username_here' )@define( 'DB_USER', 'Admin' )@g" /var/www/html/wp-config.php
+    sed -i "s@define( 'DB_PASSWORD', 'password_here' )@define( 'DB_PASSWORD', 'Admin123' )@g" /var/www/html/wp-config.php
+    sed -i "s@define( 'DB_HOST', 'localhost' )@define( 'DB_HOST', '${element(split(":", aws_db_instance.capdb.endpoint), 0)}')@g" /var/www/html/wp-config.php
+    sudo chkconfig httpd on
+    sudo service httpd start
+    sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+    sudo reboot
+
+    #sudo vi crontab
+    #* * * * * ec2-user /usr/local/bin/aws s3 sync --delete s3://capcode /var/www/html/
+    EOF
+    tags = {
+        Name = "webserver"
+    }
+}
+
