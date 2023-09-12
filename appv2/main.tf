a11f785e6d7fd6a732d8d7239b9f2a07dc083ea9
VictorA07 <victor.adepoju@cloudhight.com>
2023-09-12 16:28:30 +0100

    updating code

--- appv2/main.tf
+++ appv2/main.tf
@@ -144,9 +144,9 @@ resource "aws_security_group" "capfrontend-SG" {
   }
 # https inbound rule
   ingress {
-    description = "HTTP"
-    from_port = var.http_port
-    to_port = var.http_port
+    description = "HTTPS"
+    from_port = var.https_port
+    to_port = var.https_port
     protocol = "tcp"
     cidr_blocks = [var.all_access_cidr]
   }
@@ -224,26 +224,25 @@ resource "aws_db_instance" "capdb" {
 
 #Creating S3 Bucket
 #Creating S3 media bucket
-resource "aws_s3_bucket" "capmedia" {
-  bucket = "capmedia"
+resource "aws_s3_bucket" "mediabuk" {
+  bucket = "mediabuk"
   force_destroy = true
   tags = {
-      Name = "cap-media"
+      Name = "cap-mediabuk"
   }
 }
 
 #aws_s3_bucket_ownership_controls
-resource "aws_s3_bucket_ownership_controls" "capmedia-ct" {
-    bucket = aws_s3_bucket.capmedia.id
+resource "aws_s3_bucket_ownership_controls" "mediabuk-ct" {
+    bucket = aws_s3_bucket.mediabuk.id
     rule {
-      object_ownership = "BucketOwnerEnforced"
+      object_ownership = "BucketOwnerPreferred"
     }
-    depends_on = [ aws_s3_bucket_public_access_block.capmedia-pab ]
 }
 
 #aws_s3_bucket_public_access_block"
-resource "aws_s3_bucket_public_access_block" "capmedia-pab" {
-    bucket = aws_s3_bucket.capmedia.id
+resource "aws_s3_bucket_public_access_block" "mediabuk-pab" {
+    bucket = aws_s3_bucket.mediabuk.id
     block_public_acls = false
     block_public_policy = false
     ignore_public_acls = false
@@ -252,30 +251,30 @@ resource "aws_s3_bucket_public_access_block" "capmedia-pab" {
 
 #aws_s3_bucket_acl
 resource "aws_s3_bucket_acl" "cap-media-acl" {
-    bucket = aws_s3_bucket.capmedia.id
-    depends_on = [ aws_s3_bucket_ownership_controls.capmedia-ct]
-    acl = "public-read"
+    bucket = aws_s3_bucket.mediabuk.id
+    depends_on = [ aws_s3_bucket_ownership_controls.mediabuk-ct, aws_s3_bucket_public_access_block.mediabuk-pab ]
+    acl = "public-read"    
 }
 
 #Creating Bucket policy
 resource "aws_s3_bucket_policy" "cap-media-policy" {
-    bucket = aws_s3_bucket.capmedia.id
-    policy = jsonencode({
-        id = "cap-media-policy"
-        statement = [
-            {
-                Action = ["s3:GetObject", "s3:GetObjectVersion"]
-                effect = "allow"
-                principal = {
-                    AWS = "*"
-                }
-                resource = "arn:aws:s3:::capmedia/*"
-                sid = "PublicReadGetObject"
-            }
-        ]
-        Version = "2012-10-17"
-    })
-    depends_on = [ aws_s3_bucket_public_access_block.capmedia-pab ]
+  depends_on = [ aws_s3_bucket_acl.cap-media-acl ]
+  bucket = aws_s3_bucket.mediabuk.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id = "capMediaPolicy"
+    Statement = [
+      {
+        Action = ["s3:GetObject", "s3:GetObjectVersion"]
+        Effect = "Allow"
+        Principal = {
+            AWS = "*"
+        }
+        Resource = "arn:aws:s3:::mediabuk/*"
+        Sid = "PublicReadGetObject"
+      }
+    ]
+  })
 }
 
 #Creating S3 code bucket
@@ -287,51 +286,88 @@ resource "aws_s3_bucket" "capcode" {
     }
 }
 
+#aws_s3_bucket_acl code bucket
+resource "aws_s3_bucket_acl" "cap-code-acl" {
+    bucket = aws_s3_bucket.capcode.id
+    depends_on = [ aws_s3_bucket_ownership_controls.capcode-ct ]
+    acl = "private"    
+}
+
+#aws_s3_bucket_ownership_controls code 
+resource "aws_s3_bucket_ownership_controls" "capcode-ct" {
+    bucket = aws_s3_bucket.capcode.id
+    rule {
+      object_ownership = "BucketOwnerPreferred"
+    }
+}
+
 #Creating log Bucket
 resource "aws_s3_bucket" "cap-log" {
     bucket = "my-cap-log"
     force_destroy = true
+    tags = {
+        Name = "cap-log"
+    }
 }
 resource "aws_s3_bucket_acl" "cap-log-acl" {
     bucket = aws_s3_bucket.cap-log.id
-    depends_on = [ aws_s3_bucket_ownership_controls.cap-log-ct ]
-    acl = "log-delivery-write"
+    depends_on = [ aws_s3_bucket_ownership_controls.cap-log-ct, aws_s3_bucket_public_access_block.caplog-pab ]
+    acl = "private"
 }
+
+#aws_s3_bucket_public_access_block"
+resource "aws_s3_bucket_public_access_block" "caplog-pab" {
+    bucket = aws_s3_bucket.cap-log.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false   
+}
+
 resource "aws_s3_bucket_ownership_controls" "cap-log-ct" {
     bucket = aws_s3_bucket.cap-log.id
     rule {
-      object_ownership = "BucketOwnerEnforced"
+      object_ownership = "BucketOwnerPreferred"
     }
-    #depends_on = [ aws_s3_bucket_acl.cap-log-acl ] 
 }
 
 #Creating log bucket policy
 resource "aws_s3_bucket_policy" "log-policy" {
-    bucket = aws_s3_bucket.cap-log.id
-    policy = jsonencode({
-    id = "cap-log-policy"
-        statement = [
-            {
-                Action = ["s3:GetObject", "s3:GetObjectVersion", "s3:PutObject"]
-                effect = "allow"
-                principal = {
-                    AWS = "*"
-                }
-                resource = "arn:aws:s3:::cap-log/*"
-                sid = "PublicReadGetObject"
-            }
-        ]
-        Version = "2012-10-17"
-    })  
+  depends_on = [ aws_s3_bucket_acl.cap-log-acl ]
+  bucket = aws_s3_bucket.cap-log.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id = "cap-log-policy"
+    Statement = [
+        {
+          Action = ["s3:GetObject", "s3:GetObjectVersion"]
+          Effect = "Allow"
+          Principal = {
+              AWS = "*"
+          }
+          Resource = "${aws_s3_bucket.cap-log.arn}/*"
+          Sid = "PublicReadGetObject"
+        },
+        {
+          Action = ["s3:PutObject"]
+          Effect = "Allow"
+          Principal = {
+              AWS = "*"
+          }
+          Resource = "${aws_s3_bucket.cap-log.arn}/*"
+          Sid = "PublicWritePutObject"
+        }
+      ] 
+  })  
 }
 
 #Creating IAM role and IAM instnace profile
 
-resource "aws_iam_role" "cap-S3IAM" {
-    name = "cap-S3IAM"
-    assume_role_policy = data.aws_iam_policy_document.cap-S3IAM-rol.json
+resource "aws_iam_role" "cap-IAMR" {
+    name = "cap-IAMR"
+    assume_role_policy = data.aws_iam_policy_document.cap-IAMR-rol.json
 }
-data "aws_iam_policy_document" "cap-S3IAM-rol" {
+data "aws_iam_policy_document" "cap-IAMR-rol" {
     statement {
       effect = "Allow"
 
@@ -345,12 +381,12 @@ data "aws_iam_policy_document" "cap-S3IAM-rol" {
 }
 
 #aws_iam_policy
-resource "aws_iam_policy" "S3IAM-policy" {
-  name = "S3IAM-policy"
+resource "aws_iam_policy" "IAMR-policy" {
+  name = "IAMR-policy"
   description = "Access to Ec2 instnace and S3 bucket"
-  policy = data.aws_iam_policy_document.cap-S3IAM-pol.json
+  policy = data.aws_iam_policy_document.cap-IAMR-pol.json
 }
-data "aws_iam_policy_document" "cap-S3IAM-pol" {
+data "aws_iam_policy_document" "cap-IAMR-pol" {
   statement {
     effect = "Allow"
     actions = ["s3:*"]
@@ -358,14 +394,14 @@ data "aws_iam_policy_document" "cap-S3IAM-pol" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "cap-S3IAM" {
-  role = aws_iam_role.cap-S3IAM.name
-  policy_arn = aws_iam_policy.S3IAM-policy.arn
+resource "aws_iam_role_policy_attachment" "cap-IAMR" {
+  role = aws_iam_role.cap-IAMR.name
+  policy_arn = aws_iam_policy.IAMR-policy.arn
 }
 
-resource "aws_iam_instance_profile" "cap-S3IAM" {
-  name = "cap-S3IAM"
-  role = aws_iam_role.cap-S3IAM.name 
+resource "aws_iam_instance_profile" "cap-IAMR" {
+  name = "cap-IAMR"
+  role = aws_iam_role.cap-IAMR.name 
 }   
 
 #Wordpress configuration and spinning up instance
@@ -382,7 +418,7 @@ resource "aws_instance" "webserver" {
     key_name = var.keyname
     subnet_id = aws_subnet.cappub1.id
     vpc_security_group_ids = [aws_security_group.capfrontend-SG.id]
-    iam_instance_profile = "aws_iam_instance_profile.cap-S3IAM.id"
+    iam_instance_profile = aws_iam_instance_profile.cap-IAMR.id
     associate_public_ip_address = true
     user_data = templatefile(var.wordpress, {
     database_name = var.database_name,
@@ -399,11 +435,11 @@ resource "aws_instance" "webserver" {
 
 #cloud front distribution
 locals {
-  s3_origin_id = "aws_s3_bucket.capmedia.bucket"
+  s3_origin_id = "aws_s3_bucket.mediabuk.bucket"
 }
 resource "aws_cloudfront_distribution" "cap-cldfront" {
     origin {
-      domain_name = aws_s3_bucket.capmedia.bucket_domain_name
+      domain_name = aws_s3_bucket.mediabuk.bucket_regional_domain_name
       origin_id = local.s3_origin_id
     }
     enabled = true
@@ -412,10 +448,10 @@ resource "aws_cloudfront_distribution" "cap-cldfront" {
 
     # logging_config {
     #     include_cookies = false
-    #     bucket = "cap-log.s3.amazonaws.com"
+    #     bucket = aws_s3_bucket.cap-log.bucket_regional_domain_name
     #     prefix = "mylog"
     #   }
-    #   aliases = [var.domain_name]
+      # aliases = [var.domain_name]
 
     default_cache_behavior {
       allowed_methods = ["GET", "POST", "PUT", "DELETE", "PATCH","HEAD", "OPTIONS"]
@@ -432,6 +468,27 @@ resource "aws_cloudfront_distribution" "cap-cldfront" {
       min_ttl = 0
       default_ttl = 0
       max_ttl = 600
+    }
+    ordered_cache_behavior {
+    path_pattern     = "mediabuk/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
     }
     price_class = "PriceClass_All"
     restrictions {
@@ -463,7 +520,7 @@ resource "aws_ami_from_instance" "cap-ami" {
 #Sleep time to delay AMI resoures creation
 resource "time_sleep" "EC2-wait-time" {
     depends_on = [ aws_instance.webserver ]
-    create_duration = "300s"  
+    create_duration = "120s"  
 }
 #Create Target group
 resource "aws_lb_target_group" "cap-tg" {
@@ -499,8 +556,20 @@ resource "aws_lb_listener" "cap-lb-listner" {
     load_balancer_arn = aws_lb.cap-lb.arn
     port = "80"
     protocol = "HTTP"
-    #ssl_policy = ""  #when using https
-    #certificate_arn = ""
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.cap-tg.arn
+    }  
+    #certificate_arn = aws_acm_certificate_validation.cap-acm-cert.certificate_arn
+}
+
+#Lb listner for https
+resource "aws_lb_listener" "cap-lb-listner-htps" {
+    load_balancer_arn = aws_lb.cap-lb.arn
+    port = var.https_port
+    protocol = "HTTPS"
+    ssl_policy = "ELBSecurityPolicy-2016-08"
+    certificate_arn = "${aws_acm_certificate.cap-acm-cert.arn}"
     default_action {
       type = "forward"
       target_group_arn = aws_lb_target_group.cap-tg.arn
@@ -512,7 +581,7 @@ resource "aws_launch_configuration" "cap-asg-lc" {
     name = "cap-asg-lc"
     image_id = aws_ami_from_instance.cap-ami.id
     instance_type = var.instance-type
-    iam_instance_profile = aws_iam_instance_profile.cap-S3IAM.arn
+    iam_instance_profile = aws_iam_instance_profile.cap-IAMR.arn
     associate_public_ip_address = true
     security_groups = [aws_security_group.capfrontend-SG.id]
     key_name = var.keyname
@@ -553,13 +622,13 @@ resource "aws_autoscaling_attachment" "cap-asg-attachment" {
 #Creating ASG policy
 resource "aws_autoscaling_policy" "cap-asg-policy" {
     name = "cap-asg-policy"
-    scaling_adjustment = 4
+    #scaling_adjustment = 4
     adjustment_type = "ChangeInCapcity"
     autoscaling_group_name = aws_autoscaling_group.cap-asg.name
     policy_type = "TargetTrackingScaling"
     target_tracking_configuration {
       predefined_metric_specification {
-        predefined_metric_type = "ASGAverageCPUUtilzation"
+        predefined_metric_type = "ASGAverageCPUUtilization"
       }
       target_value = 30.0
     }
@@ -569,7 +638,8 @@ resource "aws_autoscaling_policy" "cap-asg-policy" {
 #Hosted Zone
 #Create Hosted zone
 data "aws_route53_zone" "cap-HZ" {
-    name = "greatminds.sbs"  
+    name = var.domain_name 
+    private_zone = false
 }
 
 #Create an record
@@ -581,11 +651,44 @@ resource "aws_route53_record" "cap-www" {
     alias {
       name = aws_lb.cap-lb.dns_name 
       zone_id = aws_lb.cap-lb.zone_id
-      evaluate_target_health = true
+      evaluate_target_health = false
     }
 }
 
+#Route53 ACM validation
+resource "aws_route53_record" "cap-www-acm" {
+  for_each = {
+    for dvo in aws_acm_certificate.cap-acm-cert.domain_validation_options :dvo.domain_name=> {
+      name = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name = each.value.name
+  records = [each.value.record]
+  ttl = 60
+  type = each.value.type
+  zone_id = data.aws_route53_zone.cap-HZ.zone_id
+}
+
 #Create ACM certificate and validate SSL
+resource "aws_acm_certificate" "cap-acm-cert" {
+  domain_name = var.domain_name
+  validation_method = "DNS"
+  subject_alternative_names = ["*.greatminds.sbs"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#Create ACM certificate and validation SSL
+resource "aws_acm_certificate_validation" "cap-acm-cert" {
+  certificate_arn = aws_acm_certificate.cap-acm-cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cap-www-acm :record.fqdn]
+}
 
 #Monitoring
 #Create SNS topic and subscription
@@ -594,7 +697,7 @@ resource "aws_sns_topic" "cap-update" {
   delivery_policy = <<EOF
 {
   "http":{
-    "defaultHealthRetryPolicy": {
+    "defaultHealthyRetryPolicy": {
       "minDelayTarget":20,
       "maxDelayTarget":20,
       "numRetries" : 3,
@@ -626,25 +729,23 @@ resource "aws_sns_topic_subscription" "cap-sns-sub" {
 resource "aws_cloudwatch_dashboard" "cap-ec2-dashboard" {
   dashboard_name = "cap-ec2-dashboard"
   dashboard_body = jsonencode({
-    widget = [
+    widgets = [
       {
         type = "metric"
+        x = 0
+        y = 0
+        width = 12
+        height = 6
         properties = {
-          metric = [
-              [ "AWS/EC2", "CPUUtilization", "InstanceId", "${aws_instance.webserver.id}", {"label": "Average CPU utilization"}]
+          metrics = [
+            [ "AWS/EC2", "CPUUtilization", "InstanceId", "${aws_instance.webserver.id}", {"label": "Average CPU utilization"}]
           ]
           period = 300
           stat = "Average"
           view = "timeSeries"
           region = "eu-north-1"
-          title = "Average CPU Ultilization"
+          title = "Average CPU Utilization"
           stacked = false
-          yAxis = {
-            left = {
-              label = "Percentage"
-              showUnit = true
-            }
-          }
         }
       },
     ]
@@ -654,25 +755,23 @@ resource "aws_cloudwatch_dashboard" "cap-ec2-dashboard" {
 resource "aws_cloudwatch_dashboard" "cap-asg-dashboard" {
   dashboard_name = "cap-asg-dashboard"
   dashboard_body = jsonencode({
-    widget = [
+    widgets = [
       {
         type = "metric"
+        x = 0
+        y = 0
+        width = 12
+        height = 6
         properties = {
-          metric = [
+          metrics = [
               [ "AWS/EC2", "CPUUtilization", "AutoScalingGroupName", "${aws_autoscaling_group.cap-asg.id}", {"label": "Average CPU utilization"}]
           ]
           period = 300
           stat = "Average"
           view = "timeSeries"
           region = "eu-north-1"
-          title = "Average CPU Ultilization"
+          title = "Average CPU Utilization"
           stacked = false
-          yAxis = {
-            left = {
-              label = "Percentage"
-              showUnit = true
-            }
-          }
         }
       },
     ]
@@ -689,7 +788,10 @@ resource "aws_cloudwatch_metric_alarm" "cap-ec2-cloudwatch-alarm" {
   statistic = "Average"
   threshold = 80
   alarm_description = "This metric monitors EC2 CPU utilization"
-  insufficient_data_actions = []  
+  dimensions = {
+    InstanceId = aws_instance.webserver.id
+  }
+  alarm_actions = [aws_sns_topic.cap-update.arn] 
 }
 #Create cloud watch metric for autoscaling group
 
@@ -706,7 +808,7 @@ resource "aws_cloudwatch_metric_alarm" "cap-asg-cloudwatch-alarm" {
     AutoScalingGroupName = aws_autoscaling_group.cap-asg.name
   }
   alarm_description = "This metric monitors EC2 CPU utilization"
-  alarm_actions = [aws_autoscaling_policy.cap-asg-policy.arn]  
+  alarm_actions = [aws_sns_topic.cap-update.arn]  
 }
 
 
